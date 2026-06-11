3f8d7ad66894be27e84f0c9df4c9886149996c7d
dawn <bqm3010@gmail.com>
2026-06-12 00:36:52 +0700

    a

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,4 +1,4 @@
 export const PUBLIC_SITE_URL =
-  (import.meta.env.VITE_PUBLIC_SITE_URL || "https://couponza.com").replace(/\/+$/, "");
+  (import.meta.env.VITE_PUBLIC_SITE_URL || "https://couponzas.com").replace(/\/+$/, "");
 
 export const PUBLIC_SITE_HOST = new URL(PUBLIC_SITE_URL).hostname;
